78a2aac1ad0b8fc48245ee4a1d413ba0acb069ef
Dev Sakalley <81983266+Dev2704@users.noreply.github.com>
2026-06-25 20:59:59 +0530

    Commited Changes

--- todo.js
+++ todo.js
@@ -1,20 +1,79 @@
-const taskList = [];
+let taskList = [];
 
 const input = document.getElementById("taskInput");
 const button = document.getElementById("btn");
 const tasks = document.getElementById("taskShow");
 
+function saveTasks(){
+    localStorage.setItem("tasks", JSON.stringify(taskList));
+}
+
+function loadTasks(){
+    const savedTasks = localStorage.getItem("tasks");
+
+    
+    if (savedTasks) {
+        taskList = JSON.parse(savedTasks);
+    }
+
+}
+
+let nextId = 1;
+
+for (const task of taskList){
+    if (task.id >=nextId){
+        nextId = task.id + 1;
+    }
+}
+
+function render(){
+    tasks.innerHTML = "";
+    for (const task of taskList){
+        tasks.innerHTML += `
+            <li class="${task.completed ? "completed" : ""}">
+            <span>${task.task}</span>
+
+            <div class="actions">
+                <button class="complete-btn" onclick="complete(${task.id})">✓</button>
+                <button class="delete-btn" onclick="deleteTask(${task.id})">✕</button>
+            </div>
+            </li>
+        `;
+    }
+}
+
+function complete(id){
+    const completedTask = taskList.find(task => task.id == id);
+    completedTask.completed = true;
+
+    saveTasks();
+    render();
+}
+
+function deleteTask(id){
+    taskList = taskList.filter(task => task.id !== id);
+
+    saveTasks();
+    render();
+
+}
+
 function addTask(){
     if (input.value.trim() == ""){
         tasks.innerHTML = "Task can't be empty!!"
     }else{
-        taskList.push(input.value);
-        input.value = "";
-        tasks.innerHTML = "";
+        const newTask = {
+            id : nextId,
+            task : input.value,
+            completed: false
+        };
+        nextId++;
 
-        for (const task of taskList){
-            tasks.innerHTML += `<li>${task}</li>`;
-        }
+        taskList.push(newTask);
+        input.value = "";
+
+        saveTasks();
+        render();
     }
     
 }
@@ -25,3 +84,6 @@ input.addEventListener("keypress", (event) =>{
         addTask();
     }
 })
+
+loadTasks();
+render();
